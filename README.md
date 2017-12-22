eb18b0db8ed979173909741bc4cad11e55ec54d6
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2017-12-23 01:31:10 +0200

    Updated the README, created test-app

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Chatbot-NodeJS
 Simple chatbot using Node.JS
+
+# Installation
+
+```
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y build-essential
+```
+If you have problems, check this: https://nodejs.org/en/download/package-manager/
+
+# The Chatbot App
+
+# Run the App
+```
+node app.js
+```
+
+# Node.JS tutorial
+
+basics: https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBMdkKFn3HasZnnAqVjzHn_
+
+
+
---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+/*function PlaceOrder(OrderNumber)
+{
+
+console.log('Order number %s is placed', OrderNumber);
+ProcessOrder(function(){console.log('now processing the order')});
+console.log("Processing order number %s finished", OrderNumber);
+};
+
+function ProcessOrder(callback)
+{
+	// assuming that there is a process here taking 5 seconds to be done.
+	setTimeout(callback,5000);
+};
+
+/*function AddNUmbers(a, b)
+{
+	return a + b;
+}
+
+var student= {
+	firstname: 'abdelrahman',
+	SecondName: 'Radwan',
+	Age: 22
+};
+
+console.log(student)
+console.log(AddNUmbers(6, 7))
+
+PlaceOrder(1)
+PlaceOrder(2)
+PlaceOrder(3)
+PlaceOrder(4)
+PlaceOrder(5)
+PlaceOrder(6)
+PlaceOrder(7)
+PlaceOrder(8)*/
