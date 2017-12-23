acce681687d68d5f5abfe2c41f35b845db85e736
AbdelrahmanRadwan <abdelrahman_hamdy_radwan@yahoo.com>
2017-12-23 18:08:56 +0200

    Modefied the bot's performance

--- Chatbot.js
+++ Chatbot.js
@@ -7,6 +7,7 @@ module.exports =
 		this.Bot_Age = 22;
 		this.Bot_Name = "Abdelrahman Radwan";
 		this.Bot_University = "Ain Shams University";
+		this.Bot_Country = "Egypt";
 		// User data
 		this.User_Age;
 		this.User_Name;
@@ -21,6 +22,14 @@ module.exports =
 		else if(message.indexOf("age") > -1 && message.indexOf("your"))
 		{
 			return "I'm " + this.Bot_Age;
+		}
+		else if (message.indexOf("how") > -1 && message.indexOf("are") && message.indexOf("you"))
+		{
+			return "I'm fine ^_^"
+		}
+		else if(message.indexOf("where") > -1 && message.indexOf("live") && message.indexOf("you"))
+		{
+			return "I live in" + this.Bot_Country;
 		}
 		return "Sorry, I didn't get it :( ";
 	}
