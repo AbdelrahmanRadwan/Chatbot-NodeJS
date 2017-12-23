377d5a2b24d5a8a3355db171f4d3db045144723e
AbdelrahmanRadwan <abdelrahman_hamdy_radwan@yahoo.com>
2017-12-23 15:25:08 +0200

    Updated the server

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
+Chatbot = require('./Chatbot');
 var stdin = process.openStdin();
 
+console.log('Start chatting with your Chatbot ... \n')
 stdin.addListener("data", function(d) {
     // note:  d is an object, and when converted to a string it will
     // end with a linefeed.  so we (rather crudely) account for that  
     // with toString() and then trim() 
-    console.log("you entered: [" + 
-        d.toString() + "]");
+    console.log("You ==> " + d.toString().trim());
+    console.log("Bot ==> "+"asdas")
   });
 
 
